f32103ff792e0903584861d348924200f66c5b9e
Yang Zhao <jszyyx@163.com>
2020-03-04 16:50:09 +0800

    open plot log

--- Miner/Enclave/PlotDisk/PlotDisk.cpp
+++ Miner/Enclave/PlotDisk/PlotDisk.cpp
@@ -37,12 +37,10 @@ void ecall_plot_disk(const char *path)
 
         save_file(g_path.c_str(), i, out_hash256, rand_data, sizeof(rand_data));
 
-        /*
-        if ((i + 1) == 1 || (i + 1) % 50 == 0 || (i + 1) == PLOT_RAND_DATA_NUM)
+        if ((i + 1) == 1 || (i + 1) % 100 == 0 || (i + 1) == PLOT_RAND_DATA_NUM)
         {
-            cfeprintf("Save file: %luG/%luM\n", now_index + 1, i + 1);
+            cfeprintf("Plot file: %luG/%luM success\n", now_index + 1, i + 1);
         }
-        */
     }
 
     /* Generate G hashs */
