f6d79f9f1bc0642e244254281d5dae59b4fd426a
Yang Zhao <jszyyx@163.com>
2020-04-08 15:12:55 +0800

    4 log module

--- Miner/App/Log/Log.cpp
+++ Miner/App/Log/Log.cpp
@@ -24,7 +24,7 @@ Log *Log::get_instance()
  * */
 Log::Log()
 {
-    this->debug = false;
+    this->debug_flag = false;
 }
 
 /**
@@ -32,7 +32,104 @@ Log::Log()
  * */
 void Log::open_debug(void)
 {
-    this->debug = true;
+    this->debug_flag = true;
 }
 
+/**
+ * @description: print information
+ * @param format -> data format 
+ * */
+void Log::info(const char *format, ...)
+{
+    this->log_mutex.lock();
+    va_list va;
+    va_start(va, format);
+    int n = vsnprintf(this->log_buf, CRUST_LOG_BUF_SIZE, format, va);
+    va_end(va);
+    std::string log_str(this->log_buf, n);
+    this->log_mutex.unlock();
+    this->base_log(log_str, CRUST_LOG_INFO_TAG);
 }
+
+/**
+ * @description: print information
+ * @param format -> data format 
+ * */
+void Log::warn(const char *format, ...)
+{
+    this->log_mutex.lock();
+    va_list va;
+    va_start(va, format);
+    int n = vsnprintf(this->log_buf, CRUST_LOG_BUF_SIZE, format, va);
+    va_end(va);
+    std::string log_str(this->log_buf, n);
+    this->log_mutex.unlock();
+    this->base_log(log_str, CRUST_LOG_WARN_TAG);
+}
+
+/**
+ * @description: print information
+ * @param format -> data format 
+ * */
+void Log::err(const char *format, ...)
+{
+    this->log_mutex.lock();
+    va_list va;
+    va_start(va, format);
+    int n = vsnprintf(this->log_buf, CRUST_LOG_BUF_SIZE, format, va);
+    va_end(va);
+    std::string log_str(this->log_buf, n);
+    this->log_mutex.unlock();
+    this->base_log(log_str, CRUST_LOG_ERR_TAG);
+}
+
+/**
+ * @description: print information
+ * @param format -> data format 
+ * */
+void Log::debug(const char *format, ...)
+{
+    if (this->debug_flag)
+    {
+        this->log_mutex.lock();
+        va_list va;
+        va_start(va, format);
+        int n = vsnprintf(this->log_buf, CRUST_LOG_BUF_SIZE, format, va);
+        va_end(va);
+        std::string log_str(this->log_buf, n);
+        this->log_mutex.unlock();
+        this->base_log(log_str, CRUST_LOG_DEBUG_TAG);
+    }
+}
+
+/**
+ * @description: print base data
+ * @param log_str -> data for logging
+ * @param tag -> log tag
+ * */
+void Log::base_log(std::string log_str, const char* tag)
+{
+    // Get timestamp
+    time_t ts;
+    struct tm time_tm, *time_tmp;
+    char time_str[64];
+    time(&ts);
+
+    time_tmp = localtime(&ts);
+    if (time_tmp == NULL)
+    {
+        perror("localtime");
+        return;
+    }
+    time_tm = *time_tmp;
+
+    // If you change this format, you may need to change the size of time_str
+    if (strftime(time_str, 64, "%b %e %Y %T", &time_tm) == 0)
+    {
+        time_str[0] = 0;
+    }
+
+    printf("[%s] [%s] %s", time_str, tag, log_str.c_str());
+}
+
+} // namespace crust
--- Miner/App/Log/Log.h
+++ Miner/App/Log/Log.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+#include <stdlib.h>
+#include <time.h>
+#include <stdarg.h>
+#include <string>
+#include <mutex>
+
+#define CRUST_LOG_BUF_SIZE  10000
+#define CRUST_LOG_INFO_TAG "INFO"
+#define CRUST_LOG_WARN_TAG "WARN"
+#define CRUST_LOG_ERR_TAG "ERROR"
+#define CRUST_LOG_DEBUG_TAG "DEBUG"
+
 namespace crust
 {
 
@@ -12,9 +24,16 @@ public:
     static Log *log;
     static Log *get_instance();
     void open_debug(void);
+    void info(const char *format, ...);
+    void warn(const char *format, ...);
+    void err(const char *format, ...);
+    void debug(const char *format, ...);
 
 private:
-    bool debug;
+    void base_log(std::string log_data, const char* tag);
+    bool debug_flag;
+    char log_buf[CRUST_LOG_BUF_SIZE];
+    std::mutex log_mutex;
     Log(void);
 };
 
