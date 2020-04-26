e9885408e2b911b50977cb21ea3dc76128c0ecd5
Yang Zhao <jszyyx@163.com>
2020-04-26 14:11:22 +0800

    mute entering directory

--- test/CommonTest.cpp
+++ test/CommonTest.cpp
@@ -1,14 +1,12 @@
-#define BOOST_TEST_MODULE format_utils_test
+#define BOOST_TEST_MODULE common_test
 #include "Common.h"
 #include <boost/test/included/unit_test.hpp>
-
-bool offline_chain_mode = false;
 
 BOOST_AUTO_TEST_SUITE(common_test)
 
 BOOST_AUTO_TEST_CASE(remove_chars_from_string_test)
 {
-    std::string for_remove = "adb.hedf.h"
+    std::string for_remove = "adb.hedf.h";
     remove_chars_from_string(for_remove, ".h");
     BOOST_CHECK(for_remove == "adbedf");
 }
--- test/FormatUtilsTest.cpp
+++ test/FormatUtilsTest.cpp
@@ -2,8 +2,6 @@
 #include "FormatUtils.h"
 #include <boost/test/included/unit_test.hpp>
 
-bool offline_chain_mode = false;
-
 BOOST_AUTO_TEST_SUITE(format_utils_test)
 
 BOOST_AUTO_TEST_CASE(char_to_int_test)
