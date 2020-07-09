14fffeb9a6fe5d407d6f8870a5cafb1d29e04b09
pangwa <pangwa@gmail.com>
2020-07-09 14:31:40 +0800

    runner should install sgx drivers
    should ensure aesm service is running.
    
    Signed-off-by: pangwa <pangwa@gmail.com>

--- docker/tee-runner/start_tee.sh
+++ docker/tee-runner/start_tee.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+echo "starting tee"
+
 if [ ! -f /opt/crust-installed ]; then
     echo 'install tee dependencies...'
     /opt/crust-bin/crust-tee/install.sh
@@ -12,4 +14,30 @@ if [ ! -f /opt/crust-installed ]; then
     fi
 fi
 
-/opt/crust/crust-tee/bin/crust-tee
+crustdir=/opt/crust
+crustteedir=$crustdir/crust-tee
+crust_env_file=$crustteedir/etc/environment
+inteldir=/opt/intel
+
+source $crust_env_file
+export LD_LIBRARY_PATH=LD_LIBRARY_PATH:$inteldir/libsgx-enclave-common/aesm
+
+#
+# ensure aesm service is running
+# otherwise enclave will not be initialized
+if pgrep -x "aesm_service" > /dev/null
+then
+    echo "aesm service running"
+else
+    echo "aesm service not running, starting it"
+    $inteldir/libsgx-enclave-common/aesm/aesm_service &
+    echo "wait 5 seconds for aesm service fully start"
+    sleep 5
+fi
+
+
+APP_ARGS=${TEE_ARGS:-""}
+
+echo "run tee with arguments: $APP_ARGS"
+
+/opt/crust/crust-tee/bin/crust-tee $APP_ARGS
