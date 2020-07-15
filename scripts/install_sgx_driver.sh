8fb123947eadce2d83248e7703f04d7fb08b2f27
LowEntropyBody <jszyyx@163.com>
2020-07-15 16:29:49 +0800

    add kmod in sgx driver shell

--- scripts/install_sgx_driver.sh
+++ scripts/install_sgx_driver.sh
@@ -8,7 +8,8 @@ verbose INFO "Apt-get update..."
 apt-get update 
 
 verbose INFO "Installing denpendencies..."
-apt-get install -y build-essential linux-headers-`uname -r`
+apt-get install -y build-essential kmod linux-headers-`uname -r`
 
 verbose INFO "Installing sgx driver..."
 ./$basedir/resource/sgx_linux_x64_driver_2.6.0_4f5bb63.bin
+
