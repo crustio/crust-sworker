b13205ad163793e553be355274b7c82d49067e97
LowEntropyBody <jszyyx@163.com>
2020-07-22 14:19:39 +0800

    fix some error (#187)

--- src/include/Resource.h
+++ src/include/Resource.h
@@ -2,7 +2,6 @@
 
 #define CRUST_INST_DIR      "/opt/crust/crust-tee/" VERSION
 #define ENCLAVE_FILE_PATH   CRUST_INST_DIR "/etc/enclave.signed.so"
-#define CONFIG_FILE_PATH    CRUST_INST_DIR "/etc/Config.json"
 
 #define HASH_LENGTH 32
 #define SEAL_BLOCK_MAX_SIZE 2097152 /* 2*1024*1024 */
