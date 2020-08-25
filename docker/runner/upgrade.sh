c3aad61989bbf80ae10f6f5f6eeea11fca99dc79
Yang Zhao <jszyyx@163.com>
2020-08-25 13:02:16 +0800

    install jq

--- docker/runner/upgrade.sh
+++ docker/runner/upgrade.sh
@@ -1,52 +1,5 @@
 #!/bin/bash
 
-### 3 params: json, key, defaultValue
-function getJsonValuesByAwk() {
-    awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
-        foundKeyCount = 0
-        while (length(json) > 0) {
-            # pos = index(json, "\""key"\"");
-            pos = match(json, "\""key"\"[ \\t]*?:[ \\t]*");
-            if (pos == 0) {if (foundKeyCount == 0) {print defaultValue;} exit 0;}
-
-            ++foundKeyCount;
-            start = 0; stop = 0; layer = 0;
-            for (i = pos + length(key) + 1; i <= length(json); ++i) {
-                lastChar = substr(json, i - 1, 1)
-                currChar = substr(json, i, 1)
-
-                if (start <= 0) {
-                    if (lastChar == ":") {
-                        start = currChar == " " ? i + 1: i;
-                        if (currChar == "{" || currChar == "[") {
-                            layer = 1;
-                        }
-                    }
-                } else {
-                    if (currChar == "{" || currChar == "[") {
-                        ++layer;
-                    }
-                    if (currChar == "}" || currChar == "]") {
-                        --layer;
-                    }
-                    if ((currChar == "," || currChar == "}" || currChar == "]") && layer <= 0) {
-                        stop = currChar == "," ? i : i + 1 + layer;
-                        break;
-                    }
-                }
-            }
-
-            if (start <= 0 || stop <= 0 || start > length(json) || stop > length(json) || start >= stop) {
-                if (foundKeyCount == 0) {print defaultValue;} exit 0;
-            } else {
-                print substr(json, start, stop - start);
-            }
-
-            json = substr(json, stop + 1, length(json) - stop)
-        }
-    }'
-}
-
 while true ; do
     case "$1" in
         -c)
@@ -69,23 +22,15 @@ while true ; do
     esac
 done
 
-if [ x"$config_file" == x"" ]; then
+if [ x"$config_file" = x"" ]; then
     echo "please give right config file"
     exit -1
 fi
 
-config=`cat $config_file`
-chain_config=`getJsonValuesByAwk "$config" "chain" "{}"`
-api_base_url=`getJsonValuesByAwk "$chain_config" "base_url" ""`
-sworker_base_url=(`getJsonValuesByAwk "$config" "base_url" ""`)
+api_base_url=`cat $config_file | jq .chain.base_url`
+sworker_base_url=`cat $config_file | jq .base_url`
 
-if [ x"${sworker_base_url[0]}" == x"$api_base_url" ]; then
-sworker_base_url=${sworker_base_url[1]}
-else
-sworker_base_url=${sworker_base_url[0]}
-fi
-
-if [ x"$api_base_url" == x"" ] || [ x"$sworker_base_url" == x"" ]; then
+if [ x"$api_base_url" = x"" ] || [ x"$sworker_base_url" = x"" ]; then
     echo "please give right config file"
     exit -1
 fi
@@ -95,6 +40,7 @@ sworker_base_url=`echo "$sworker_base_url" | sed -e 's/^"//' -e 's/"$//'`
 
 while :
 do
+
 system_health=`curl $api_base_url/system/health 2>/dev/null`
 if [ x"$system_health" = x"" ]; then
     echo "please run crust chain and api"
@@ -102,14 +48,14 @@ if [ x"$system_health" = x"" ]; then
     continue
 fi
 
-is_syncing=`getJsonValuesByAwk $system_health "isSyncing" ""`
-if [ x"$is_syncing" == x"" ]; then
+is_syncing=`echo $system_health | jq .isSyncing`
+if [ x"$is_syncing" = x"" ]; then
     echo "crust api dose not connet to crust chain"
     sleep 10
     continue
 fi
 
-if [ x"$is_syncing" == x"true" ]; then
+if [ x"$is_syncing" = x"true" ]; then
     echo "crust chain is syncing"
     sleep 10
     continue
@@ -130,8 +76,8 @@ if [ x"$id_info" = x"" ]; then
     sleep 10
     continue
 fi
-id_info=`echo $id_info | sed 's/ //g'`
-mrenclave=`getJsonValuesByAwk $id_info "mrenclave" ""`
+
+mrenclave=`echo $id_info | jq .mrenclave`
 if [ x"$mrenclave" = x"" ]; then
     echo "waiting sworker ready"
     sleep 10
