d357bc496139dcd9985af93ff82e26e6301ef39f
tonycode2012 <tonycode2012@126.com>
2021-05-24 17:27:47 +0800

    Fix annotation

--- src/enclave/workload/Workload.cpp
+++ src/enclave/workload/Workload.cpp
@@ -6,7 +6,8 @@ sgx_thread_mutex_t g_upgrade_status_mutex = SGX_THREAD_MUTEX_INITIALIZER;
 
 Workload *Workload::workload = NULL;
 
-/** * @desination: Single instance class function to get instance
+/**
+ * @desination: Single instance class function to get instance
  * @return: Workload instance
  */
 Workload *Workload::get_instance()
