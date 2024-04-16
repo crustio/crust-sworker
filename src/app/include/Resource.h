ba7f75c5bc8076b8a8221e94556e7675387f1ff6
BrianWu <wuhaixian1984@163.com>
2024-04-16 12:44:45 +0800

    Bump version from 1.1.1 to 1.1.2 changeset#2, update the version number in the source code and md docs.

--- src/app/include/Resource.h
+++ src/app/include/Resource.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include "Parameter.h"
 
-#define VERSION "1.1.1"
+#define VERSION "1.1.2"
 
 #define CRUST_INST_DIR      "/opt/crust/crust-sworker/" VERSION
 #define ENCLAVE_FILE_PATH   CRUST_INST_DIR "/etc/enclave.signed.so"
